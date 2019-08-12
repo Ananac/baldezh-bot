efa3ba2d868f01baeafce7020a93cfe60624c2d1
denis.bazylev <denis.bazylev@accenture.com>
2019-08-12 16:45:35 +0300

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf('860469083:AAElj7TvrvxwtOghWazeuucmticDiLDR_38')
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
