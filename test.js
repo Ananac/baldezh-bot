28cc3c0f468bbde55ad288eb272f8bd4821ade23
denis.bazylev <denis.bazylev@accenture.com>
2019-11-13 17:32:15 +0300

    test

--- test.js
+++ test.js
@@ -1,40 +1,19 @@
-const cloudscraper = require("cloudscraper");
-const cheerio = require("cheerio");
-var pag = [];
+// const cloudscraper = require("cloudscraper");
+// const cheerio = require("cheerio");
 
-const page = Math.floor(Math.random() * 502);
-const options = {
-  method: "GET",
-  url: `https://prodota.ru/forum/index.php?showtopic=215780&page=${page}`
-};
+const http = require("http");
 
-  cloudscraper(options).then(html => {
-    let $ = cheerio.load(html);
-    const links = $('.post.entry-content span');
+// const https = require("https");
+// var pag = [];
 
-    $(links).each(function(i, link) {
-      var sop = $(this).find(".bbc_img").attr('src');   
-      if ((sop !== "") & (sop !== undefined) & (sop !== /prodota/gi)) {
-        pag[i] = sop;
-        console.log(sop);        
-    console.log(pag.length);
-      }
-    });})
+http.get("http://rzhunemogu.ru/RandJSON.aspx?CType=11", res => {
+  console.log("statusCode:", res.statusCode);
+  console.log("headers:", res.headers);
 
-    console.log("voooot" + pag[4]);
-    console.log(pag.length);
-
-// scrape(function() {
-//   randomComment();
-// });
-
-// const randomComment = function() {
-//   const x = Math.floor(Math.random() * pag.length);
-//   console.log("x = " + x);
-//   if ((pag[x] === undefined) | (pag[x] === "") | (pag[x] === /prodota/gi)) {
-//     console.log("Empty comment");
-//     randomComment();
-//   } else {
-//     console.log(pag[x]);
-//   }
-// };
+  res.on("data", d => {
+    process.stdout.write(d);
+    const obj = JSON.parse(d);
+    const memeUrl = obj.content;
+    console.log(memeUrl);
+  });
+});
