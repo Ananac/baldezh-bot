268e3cd230da470cc8f232b461fd17b8ad09d532
denis.bazylev <denis.bazylev@accenture.com>
2019-11-13 18:26:51 +0300

    test

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.hears(/пд/i, ctx => {
   console.log("пд");
   try {
     const scrape = function(callback) {
-      let page = Math.floor(Math.random() * 204s);
+      let page = Math.floor(Math.random() * 204);
       const options = {
         method: "GET",
         url: `https://prodota.ru/forum/index.php?showtopic=216714&page=${page}`
