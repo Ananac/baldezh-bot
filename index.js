700708945fc6634a4a8514474a6fb891230111ad
denis.bazylev <denis.bazylev@accenture.com>
2019-11-19 11:27:59 +0300

    test

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ bot.hears(/пд/i, ctx => {
   console.log("пд");
   try {
     const scrape = function(callback) {
-      let page = Math.floor(Math.random() * 204);
+      let page = Math.floor(Math.random() * 220);
       const options = {
         method: "GET",
         url: `https://prodota.ru/forum/index.php?showtopic=216714&page=${page}`
