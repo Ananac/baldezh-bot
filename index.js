d4aecaf661558cc53914d1c2ba27c8f066e114d8
Bazylev <denis.bazylev@accenture.com>
2021-11-19 15:30:15 +0300

    test

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ bot.hears(/артом/gi, (ctx) => {
   console.log("артом");
   try {
     today = new Date();
-    const artemIsBack = new Date(2020, 7, 31);
+    const artemIsBack = new Date(2020, 10, 10);
     const oneDay = 1000 * 60 * 60 * 24;
     const days = Math.ceil((artemIsBack.getTime() - today.getTime()) / oneDay);
     ctx.reply(
